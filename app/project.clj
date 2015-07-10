8f8610e3f5d32b6d72dbeb9c186231fb801405c4
Siim Kallas <siimkallas@gmail.com>
2015-07-10 23:08:33 +0300

    Added basic websocket setup

--- app/project.clj
+++ app/project.clj
@@ -1,6 +1,10 @@
 (defproject app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [overtone/at-at "1.2.0"]
+                 [org.clojure/tools.logging  "0.3.1"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [compojure "1.3.4"]
+                 [ring/ring-devel "1.4.0"]
+                 [ring/ring-core "1.4.0"]
+                 [http-kit  "2.1.18"]])
+
--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -1,11 +1,32 @@
-(ns app.core)
+(ns app.core
+  (:use org.httpkit.server
+        [clojure.tools.logging :only [info]]
+        overtone.at-at)
+  (:require [ring.middleware.reload :as reload]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [compojure.core :refer [defroutes GET]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def pool (mk-pool))
 
+(defn ws-handler [request]
+  (with-channel request channel
+    (info channel "connected")
+    (every 500 #(send! channel "tits") pool)
+    (on-close channel (fn [status] (info "channel closed: " status)))
+    (on-receive channel (fn [data]
+                          (info data)
+                          (send! channel data)))))
 
+(defroutes all-routes
+  (GET "/" [] "gtfo")
+  (GET "/feed" [] ws-handler)
+  (route/not-found "not found"))
 
+(defn in-dev? [args] true)
 
-
+(defn -main [& args]
+  (let [handler (if (in-dev? args)
+                 (reload/wrap-reload (site #'all-routes))
+                 (site all-routes))]
+    (run-server handler { :port 9001 })))
